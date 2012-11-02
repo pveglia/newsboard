52ca2513592f59efcdaa67d93eb11d9f5b38686d
Paolo Veglia <pveglia@anevia.com>
2012-11-02 16:36:18 +0100

    remove commented function

--- resources/public/js/news.js
+++ resources/public/js/news.js
@@ -1,16 +1,3 @@
-// function voteUp(item){
-//     xml = new XMLHttpRequest();
-//     xml.onreadystatechange=function(){
-//         if(xml.readyState == 4 && xml.status == 200){
-//             console.log(xml.responseText);
-//             document.getElementById(item).innerHTML = xml.responseText;
-//         }
-//     }
-//     xml.open("POST", "/vote");
-//     xml.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-//     xml.send("item=" + item);
-// }
-
 function voteUp(item, btn) {
     btn.disabled = true;
     $.ajax({
